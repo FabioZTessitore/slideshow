2059e4279ce38ee6390cc5dff9a56fa595edafbb
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-03-26 20:59:48 +0000

    send image to client

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(express.static('public'));
 app.use(express.static('images'));
@@ -9,8 +11,7 @@ app.use(express.static('images'));
 let images = [];
 let currentImage = -1;
 
-fs.watch('images', function (eventType, filename) {
-  /* ignore the event and read the dir */
+const readImages = function () {
   fs.readdir('images', function (err, files) {
     if (err) {
       console.log(err);
@@ -19,15 +20,29 @@ fs.watch('images', function (eventType, filename) {
 
     images = files;
     currentImage = images.length > 0 ?  0 : -1;
-
-    console.log(currentImage);
   });
+};
+
+readImages();
+
+fs.watch('images', function (eventType, filename) {
+  /* ignore the event and read the dir */
+  readImages();
 });
 
-app.listen(3000, function () {
+server.listen(3000, function () {
   console.log('slideshow server listening on port 3000');
 });
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html');
+  res.sendfile('index.html');
 });
+
+setInterval(function () {
+  if (currentImage < 0) {
+    io.emit('image', { index: currentImage });
+  } else {
+    io.emit('image', { index: currentImage, image: images[currentImage] });
+    currentImage = (currentImage+1) % images.length;
+  }
+}, 3000);
