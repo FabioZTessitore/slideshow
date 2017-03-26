cbdf7ad3e85ea140712e65e13096edf81bfb2f24
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-03-26 20:14:52 +0000

    read images dir

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.static('images'));
+
+let images = [];
+let currentImage = -1;
+
+fs.watch('images', function (eventType, filename) {
+  /* ignore the event and read the dir */
+  fs.readdir('images', function (err, files) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    images = files;
+    currentImage = images.length > 0 ?  0 : -1;
+
+    console.log(currentImage);
+  });
+});
 
 app.listen(3000, function () {
   console.log('slideshow server listening on port 3000');
